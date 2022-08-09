cfe5d81927fb7d9f1efdbc9fc0fa772268cbf6a4
Lev <levdoescode@gmail.com>
2022-08-09 16:57:44 -0500

    Add Initial commit

---
+++ 02-HackerRank/001-049/006-staircase.js
@@ -0,0 +1,12 @@
+function staircase(n) {
+  let space = "";
+  let h = "";
+  for (let i = 0; i < n; i++) {
+    h += "#";
+    for (let j = 0; j < n - i - 1; j++) space += " ";
+    console.log(space + h);
+    space = "";
+  }
+}
+
+staircase(6);
