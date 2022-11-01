339b8ebd580e6963c24a6fe2fc94a67e205946db
levdoescode <levdoescode+sites@gmail.com>
2022-11-01 12:25:26 -0500

    Challenge completed!

---
+++ 02-HackerRank/051-100/055-modified-kaprekar-numbers.js
@@ -0,0 +1,21 @@
+function kaprekarNumbers(p, q) {
+    let numbers = [];
+    for (let i = p; i <= q; i++) {
+        let numDigits = i.toString().length;
+        let squared = (i * i).toString();
+        let l = parseInt(squared.slice(0, squared.length - numDigits));
+        let r = parseInt(squared.slice(squared.length - numDigits));
+        if (l + r == i || i == 1) {
+            numbers.push(i);
+        }
+    }
+
+    if (numbers.length == 0) {
+        console.log('INVALID RANGE');
+    }
+    else {
+        console.log(numbers.join(' '));
+    }
+}
+
+kaprekarNumbers(1, 100);
