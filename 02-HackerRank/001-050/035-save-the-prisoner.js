39b935c2abfe6a3fa93fc6cc5365c780084b2fcf
levdoescode <levdoescode+sites@gmail.com>
2022-11-01 11:32:21 -0500

    Complete Save the Prisoner! challenge

--- 02-HackerRank/001-050/035-save-the-prisoner.js
+++ 02-HackerRank/001-050/035-save-the-prisoner.js
@@ -1,6 +1,11 @@
 function saveThePrisoner(n, m, s) {
-  
+    let warn = ((m % n) + s - 1) % n;
+    (warn == 0) && (warn = n);
+    return warn;
 }
 
 let arr1 = [5, 2, 1];
-saveThePrisoner(arr1[0], arr1[1], arr1[2]);
+console.log(saveThePrisoner(5, 2, 1));
+console.log(saveThePrisoner(4, 6, 2));
+console.log(saveThePrisoner(7, 19, 2)); // 6
+console.log(saveThePrisoner(3, 7, 3)); // 3
