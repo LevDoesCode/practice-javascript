b39d8c2f274953972caa0d6ee2b491fe7c8a8964
levdoescode <levdoescode+sites@gmail.com>
2022-11-09 14:36:23 -0500

    Challenge completed without using map()

---
+++ 02-HackerRank/051-100/060-cut-the-sticks.js
@@ -0,0 +1,15 @@
+function cutTheSticks(arr) {
+    let result = [];
+    let minSum = 0;
+    while (arr.length > 0) {
+        result.push(arr.length);
+        let min = Math.min(...arr);
+        console.log(min);
+        minSum += min;
+        //arr = arr.filter((val) => val - minSum - (min - minSum)> 0); // This can be simplied
+        arr = arr.filter((val) => val - min > 0);
+    }
+    return result;
+}
+
+cutTheSticks([5, 4, 4, 2, 2, 8]);
