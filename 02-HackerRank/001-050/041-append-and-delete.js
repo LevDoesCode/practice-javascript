b04a128da882c7f4625da0950de61050e17cf5ec
levdoescode <levdoescode+sites@gmail.com>
2022-11-11 03:34:09 -0500

    Challenge completed!

--- 02-HackerRank/001-050/041-append-and-delete.js
+++ 02-HackerRank/001-050/041-append-and-delete.js
@@ -1,11 +1,17 @@
-function appendAndDelete(s, t, k) {
-    for (let i = 0; i < k; i++) {
-        let thisSlice = s.slice(0, s.length - i);
-        if (t.includes(thisSlice)) {
-            if (i + (t.length - thisSlice.length) == k) {
-                return 'Yes';
-            }
-        }
+function appendAndDelete(start, end, moves) {
+    if (moves >= start.length + end.length) {
+        return 'Yes';
+    }
+    let differentAtIndex = 0;
+    while (
+        start[differentAtIndex] == end[differentAtIndex] &&
+        differentAtIndex < start.length &&
+        differentAtIndex < end.length) {
+        differentAtIndex += 1;
+    }
+    let movesNeeded = start.length - differentAtIndex + end.length - differentAtIndex;
+    if (moves == movesNeeded || moves > movesNeeded && (moves - movesNeeded) % 2 == 0) {
+        return 'Yes';
     }
     return 'No';
 }
@@ -15,3 +21,4 @@ console.log(appendAndDelete('aba', 'aba', 7));
 console.log(appendAndDelete('hackerhappy', 'hackerrank', 9));
 console.log(appendAndDelete('ashley', 'ash', 2));
 console.log(appendAndDelete('abcd', 'abcdert', 10));
+console.log(appendAndDelete('aaa', 'a', 5));
