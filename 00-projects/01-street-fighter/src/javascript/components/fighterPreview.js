41b9757ce44b80e01ac94ae564b281caae0a3eb4
levdoescode <levdoescode+sites@gmail.com>
2023-04-18 00:57:06 -0500

    Added fighter preview, name and stats

--- 00-projects/01-street-fighter/src/javascript/components/fighterPreview.js
+++ 00-projects/01-street-fighter/src/javascript/components/fighterPreview.js
@@ -8,7 +8,29 @@ export function createFighterPreview(fighter, position) {
   });
 
   // todo: show fighter info (image, name, health, etc.)
-
+  console.log(fighter);
+  if (fighter) {
+    fighterElement.appendChild(createFighterImage(fighter));
+    let nameElement = createElement({ tagName: 'p', className: 'fighter-preview___name' });
+    nameElement.innerHTML = fighter.name.toUpperCase();
+    fighterElement.appendChild(nameElement);
+    
+    let stats = createElement({ tagName: 'div', className: 'fighter-preview___stats' });
+    for (const detail in fighter) {
+      if (detail !== 'name' && detail !== '_id' && detail !== 'source') {
+        let statRow = createElement({ tagName: 'div', className: 'fighter-preview___stats-row' });
+        let statRowHead = createElement({ tagName: 'div', className: 'fighter-preview___stats-row-head' });
+        statRowHead.innerHTML = `${detail}`;
+        let statRowValue = createElement({ tagName: 'div', className: 'fighter-preview___stats-row-value' });
+        statRowValue.innerHTML = `${fighter[detail]}`;
+        statRow.appendChild(statRowHead);
+        statRow.appendChild(statRowValue);
+        stats.appendChild(statRow);
+      }
+    }
+    fighterElement.appendChild(stats);
+  }
+  
   return fighterElement;
 }
 
