66ae5321bbf4179d6dbf71837966fecfee085a03
levdoescode <levdoescode+sites@gmail.com>
2023-04-26 10:45:54 -0500

    Added fighter direction to select health bar

--- 00-projects/01-street-fighter/src/javascript/components/fighter.js
+++ 00-projects/01-street-fighter/src/javascript/components/fighter.js
@@ -1,5 +1,7 @@
 export function Fighter(fighterDetails, direction) {
     this.id = parseInt(fighterDetails._id);
+    this.direction = direction;
+    this.maxHealth = fighterDetails.health;
     this.health = fighterDetails.health;
     this.attack = fighterDetails.attack;
     this.defense = fighterDetails.defense;
