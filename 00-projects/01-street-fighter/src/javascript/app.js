d7afa042452f8ea747babff10d52d40302a9e919
levdoescode <levdoescode+sites@gmail.com>
2023-04-29 22:49:50 -0500

    Added select sound and require to click on screen

--- 00-projects/01-street-fighter/src/javascript/app.js
+++ 00-projects/01-street-fighter/src/javascript/app.js
@@ -1,5 +1,6 @@
 import { createFighters } from './components/fightersView';
 import { fighterService } from './services/fightersService';
+import { selectSound } from './components/fighterSelector';
 
 class App {
   static rootElement = document.getElementById('root');
@@ -8,16 +9,21 @@ class App {
   static async startApp() {
     try {
       App.loadingElement.style.visibility = 'visible';
+      
 
       const fighters = await fighterService.getFighters();
       const fightersElement = createFighters(fighters);
 
-      App.rootElement.appendChild(fightersElement);
+      this.loadingElement.addEventListener('click', () => {
+        selectSound.play();
+        App.rootElement.appendChild(fightersElement);
+        App.loadingElement.style.visibility = 'hidden';
+      });
     } catch (error) {
       console.warn(error);
       App.rootElement.innerText = 'Failed to load data';
     } finally {
-      App.loadingElement.style.visibility = 'hidden';
+      
     }
   }
 }
