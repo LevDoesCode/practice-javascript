70b71120ac9118f8a25b1a56ae951d641b92f98a
levdoescode <levdoescode+sites@gmail.com>
2023-05-06 17:28:52 -0500

    Added once arg to avoid multiple adds of the same event listener

--- 00-projects/01-street-fighter/src/javascript/app.js
+++ 00-projects/01-street-fighter/src/javascript/app.js
@@ -5,20 +5,20 @@ import { selectSound } from './components/fighterSelector';
 class App {
   static rootElement = document.getElementById('root');
   static loadingElement = document.getElementById('loading-overlay');
-
   static async startApp() {
     try {
       App.loadingElement.style.visibility = 'visible';
-      
-
       const fighters = await fighterService.getFighters();
       const fightersElement = createFighters(fighters);
 
-      this.loadingElement.addEventListener('click', () => {
+      const clickOverlay = function (event) {
+        selectSound.currentTime = 0;
         selectSound.play();
         App.rootElement.appendChild(fightersElement);
         App.loadingElement.style.visibility = 'hidden';
-      });
+      }
+
+      this.loadingElement.addEventListener('click', clickOverlay, {once: true});
     } catch (error) {
       console.warn(error);
       App.rootElement.innerText = 'Failed to load data';
