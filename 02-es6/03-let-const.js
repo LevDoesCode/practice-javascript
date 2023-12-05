8e68f5f336419edf4d7cf9684e62ae2ede3cb911
LevDoesCode <levdoescode@gmail.com>
2023-12-04 21:15:52 -0500

    const stuff

--- 02-es6/03-let-const.js
+++ 02-es6/03-let-const.js
@@ -1,5 +1,5 @@
 // Both let and const are block-scoped
-console.log("let and const")
+console.log("let and const");
 if (true) {
   let a1 = 1;
   const first = "Leo";
@@ -8,3 +8,33 @@ if (true) {
   // Throws an error because we cannot update variables declared with const
   first = "Roar";
 }
+
+// usually uppercase
+const MY_CONST_NAME = 'value';
+
+// we can change the values inside, but not object reference
+const ACCELERATION = 9.85;
+ACCELERATION = 10; // TypeError
+
+// consts need to be initialized
+const RED; // SyntaxError
+
+// JS cost and Objects
+const person = {age: 20};
+person.age = 30; // OK
+console.log(person.age);
+
+person = { age: 40 }; // TypeError
+
+// Freezing (shallow)
+const person = Object.freeze({age: 20});
+person.age = 30; // TypeError
+
+const company = Object.freeze({
+  name: 'Google',
+  address: {
+    street: '123 main st.',
+  }
+})
+
+// `company.address` is not immutable and we can change or add properties
